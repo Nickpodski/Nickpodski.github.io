6fe48401a25a4c588fa02740a6d77d1fca2853a0
Nickpodski <Nickpodski@gmail.com>
2021-05-15 10:49:48 -0400

    added background component

---
+++ src/components/Background/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style.css';
+
+const Background = () => {
+  return(
+    <div>
+      <video className="bg-video" src="./assets/Video/Space_loop.mp4"></video>
+    </div>
+  );
+};
+
+export default Background;
