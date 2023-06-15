f5e8a82f159a827e3bd25cc29133b761923a8ab1
Nickpodski <Nickpodski@gmail.com>
2023-06-15 16:03:14 -0400

    one more try

--- src/components/Background/index.js
+++ src/components/Background/index.js
@@ -4,7 +4,7 @@ import './style.css';
 const Background = () => {
   return(
     <div>
-      <img className="bg-video" src="assets/images/Space_loop_Moment.jpg" alt="background of stars at night!"></img>
+      <img className="bg-video" src="./assets/images/Space_loop_Moment.jpg" alt="background of stars at night!"></img>
     </div>
   );
 };
